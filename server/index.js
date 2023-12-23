2ee7c0c7b29f4d46b63e9c2fd8474284a8085ed7
Monday_David_Sarka <davidsarka242@gmail.com>
2023-12-23 01:09:18 +0100

    Imported all packages

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const mongoose = require("mongoose");
+
 const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
+app.use(express.json());
+app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send({Message: "Default Home Page"});
+})
+
+app.post("/upload", upload.single(""), (req, res) => {
+  res.send("Thanks Bobby");
 })
 
 
